c29f37ee70c720fa2a22bdf066f7b313982d2bef
KevinKirwa <kipngenokirwa9@gmail.com>
2025-04-03 15:54:23 +0300

    Update ESLint config and Vercel build settings

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -28,6 +28,14 @@ module.exports = {
     'jsx-a11y/role-supports-aria-props': 'warn',
     'jsx-a11y/aria-props': 'warn',
     'no-dupe-keys': 'warn',
+    'react/no-unescaped-entities': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/jsx-key': 'warn',
+    'react/no-unknown-property': 'off',
+    'react/no-deprecated': 'warn',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off'
   },
   settings: {
     react: {
