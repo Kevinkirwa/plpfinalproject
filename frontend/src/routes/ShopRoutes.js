09e13fa9294d8f61def5283b3b34a74db2195bdc
KevinKirwa <kipngenokirwa9@gmail.com>
2025-04-08 00:32:28 +0300

    Fix build error: Update shopRoutes export syntax in ShopRoutes.js

--- frontend/src/routes/ShopRoutes.js
+++ frontend/src/routes/ShopRoutes.js
@@ -1,29 +1,35 @@
 import ShopDashboardPage from "../pages/Shop/ShopDashboardPage";
-import ShopCreateProduct from "../pages/Shop/ShopCreateProduct";
 import ShopAllProducts from "../pages/Shop/ShopAllProducts";
-import ShopCreateEvents from "../pages/Shop/ShopCreateEvents";
-import ShopAllEvents from "../pages/Shop/ShopAllEvents";
-import ShopAllCoupouns from "../pages/Shop/ShopAllCoupouns";
-import ShopPreviewPage from "../pages/Shop/ShopPreviewPage";
-import ShopAllOrders from "../pages/Shop/ShopAllOrders";
-import ShopOrderDetails from "../pages/Shop/ShopOrderDetails";
-import ShopAllRefunds from "../pages/Shop/ShopAllRefunds";
-import ShopSettingsPage from "../pages/Shop/ShopSettingsPage";
-import ShopWithDrawMoneyPage from "../pages/Shop/ShopWithDrawMoneyPage";
-import ShopInboxPage from "../pages/Shop/ShopInboxPage";
+import CreateProduct from "../pages/Shop/CreateProduct";
+import ShopOrders from "../pages/Shop/ShopOrders";
+import ShopSettings from "../pages/Shop/ShopSettings";
+import ShopWithdrawMoney from "../pages/Shop/ShopWithdrawMoney";
 
-export {
-    ShopDashboardPage,
-    ShopCreateProduct,
-    ShopAllProducts,
-    ShopCreateEvents,
-    ShopAllEvents,
-    ShopAllCoupouns,
-    ShopPreviewPage,
-    ShopAllOrders,
-    ShopOrderDetails,
-    ShopAllRefunds,
-    ShopSettingsPage,
-    ShopWithDrawMoneyPage,
-    ShopInboxPage
-}
+const shopRoutes = [
+  {
+    path: "/dashboard",
+    element: <ShopDashboardPage />,
+  },
+  {
+    path: "/dashboard-products",
+    element: <ShopAllProducts />,
+  },
+  {
+    path: "/dashboard-create-product",
+    element: <CreateProduct />,
+  },
+  {
+    path: "/dashboard-orders",
+    element: <ShopOrders />,
+  },
+  {
+    path: "/dashboard-settings",
+    element: <ShopSettings />,
+  },
+  {
+    path: "/dashboard-withdraw-money",
+    element: <ShopWithdrawMoney />,
+  },
+];
+
+export { shopRoutes };
