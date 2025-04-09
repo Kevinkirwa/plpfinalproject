b3744436620ac4decdb6d23706184a032ee6393d
KevinKirwa <kipngenokirwa9@gmail.com>
2025-04-09 09:56:21 +0300

    Add Toaster component to Layout for toast notifications

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import ChatAssistant from "../ChatAssistant/ChatAssistant";
+import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
+      <Toaster position="top-center" reverseOrder={false} />
       <Header />
       <main className="flex-grow">
         {children}
